8269b3d3a0f179e70b0d82707e1024fae6acad9c
Maanav Rajesh <mnrajesh@uwaterloo.ca>
2025-04-25 16:54:29 -0400

    no broken posters

--- supabase/functions/generate_movies/tmdb.ts
+++ supabase/functions/generate_movies/tmdb.ts
@@ -1,5 +1,6 @@
 import { Movie, StreamingProvider } from './types.ts';
 import { getProviderLogoUrl } from './providers.ts';
+import axios from "axios";
 
 const TMDB_API_KEY = Deno.env.get('TMDB_API_KEY');
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
